8f4fa6d89f4fcbb62c75aa36c85611b059ec63a5
Frol Shpak <frol.shpak@gmail.com>
2020-08-29 15:47:15 -0400

    Implement the best solution

--- controllers/googleTechGuideController.js
+++ controllers/googleTechGuideController.js
@@ -1,54 +1,82 @@
 /**
- * BooksController
- * @return { any } functions*/
+ * Google challenges controller
+ * @return { any } functions, one per each challenge*/
 function googleTechGuideController() {
     /**
-     * Routers for books
+     * First challenge https://techdevguide.withgoogle.com/paths/foundational/find-longest-word-in-dictionary-that-subsequence-of-given-string#!
      * @param { any } req
      * @param { any } res
      * @return { any } result*/
     function challenge01(req, res) {
         const { inputString } = req.body;
         const { inputArray } = req.body;
+        const grouped = [];
+        const results = [];
 
-        const result = inputArray
-            .sort((a, b) => {
-                return b.length - a.length;
-            })
-            .find((el) => isSubstring(inputString, el));
+        inputArray.forEach((el) => {
+            grouped.push({
+                letter: el[0],
+                tuple: { word: el, index: 0 },
+            });
+        });
+        inputString.split('').forEach((ch) => {
+            grouped.forEach((g) => {
+                if (g.letter === ch) {
+                    g.tuple.index++;
+                    if (g.tuple.index === g.tuple.word.length) {
+                        g.letter = undefined;
+                        results.push(g.tuple.word);
+                    } else {
+                        g.letter = g.tuple.word[g.tuple.index];
+                    }
+                }
+            });
+        });
+        console.log(results);
+        results.sort((a, b) => {
+            return b.length - a.length;
+        });
+        console.log(results);
 
-        return res.json(result);
+        // Older solution
+        // const result = inputArray
+        //     .sort((a, b) => {
+        //         return b.length - a.length;
+        //     })
+        //     .find((el) => isSubstring(inputString, el));
+
+        return res.json(results[0]);
     }
 
     /**
-     * Routers for books
+     * Method that will call recursive one after prepering input
      * @param { any } str
      * @param { any } substr
      * @return { any } result*/
-    function isSubstring(str, substr) {
-        return isSubstringRec(str.split(''), substr.split(''), 0);
-    }
+    // function isSubstring(str, substr) {
+    //     return isSubstringRec(str.split(''), substr.split(''), 0);
+    // }
 
-    /**
-     * Routers for books
-     * @param { any } strChars
-     * @param { any } substrChars
-     * @param { any } startingIndex
-     * @return { any } result*/
-    function isSubstringRec(strChars, substrChars, startingIndex) {
-        let index = strChars.indexOf(substrChars[0]);
-        if (index >= startingIndex) {
-            substrChars.shift();
-            if (substrChars.length > 0) {
-                startingIndex = index++;
-                return isSubstringRec(strChars, substrChars, startingIndex);
-            } else {
-                return true;
-            }
-        } else {
-            return false;
-        }
-    }
+    // /**
+    //  * Recursive method for finding if one string is substring of another
+    //  * @param { any } strChars
+    //  * @param { any } substrChars
+    //  * @param { any } startingIndex
+    //  * @return { any } result*/
+    // function isSubstringRec(strChars, substrChars, startingIndex) {
+    //     let index = strChars.indexOf(substrChars[0]);
+    //     if (index >= startingIndex) {
+    //         substrChars.shift();
+    //         if (substrChars.length > 0) {
+    //             startingIndex = index++;
+    //             return isSubstringRec(strChars, substrChars, startingIndex);
+    //         } else {
+    //             return true;
+    //         }
+    //     } else {
+    //         return false;
+    //     }
+    // }
 
     return { challenge01 };
 }
--- routes/googleTechGuideRouter.js
+++ routes/googleTechGuideRouter.js
@@ -3,22 +3,34 @@ const googleTechGuideController = require('../controllers/googleTechGuideControl
 
 /**
  * Routers for books
- * @return { Router } googleTechGuideRouter*/
+ * @return { Router } Solutions for challenges from https://techdevguide.withgoogle.com */
 function routes() {
     const googleTechGuideRouter = express.Router();
     const controller = googleTechGuideController();
 
     googleTechGuideRouter.route('/challenges')
         .get((req, res) => {
-            res.status(201);
+            res.status(200);
             return res.send('Check');
         });
 
     googleTechGuideRouter
         .route('/challenges/find-longest-word-in-dictionary-that-subsequence-of-given-string')
         .get((req, res)=> {
-            res.status(201);
-            return res.send('Check');
+            res.status(200);
+            return res.send(`Given a string S and a set of words D, find the longest word in D that is a subsequence of S.
+            Word W is a subsequence of S if some number of characters, possibly zero, can be deleted from S to form W, without reordering the remaining characters.
+            Example payload for post:
+            {
+                "inputString": "abppplee",
+                "inputArray" : [ 
+                    "able", 
+                    "ale", 
+                    "apple", 
+                    "bale", 
+                    "kangaroo"
+                ]
+            }`);
         })
         .post(controller.challenge01);
 
